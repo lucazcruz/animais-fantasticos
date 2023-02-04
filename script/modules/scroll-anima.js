88a9a95135ff2f3a944e9bb47bef20c009128ec7
lucazcruz <lucaz.cruz17@gmail.com>
2023-02-04 11:57:50 -0300

    ScrollAnima refeito para melhor desempenho

--- script/modules/scroll-anima.js
+++ script/modules/scroll-anima.js
@@ -2,23 +2,40 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.alturaMedia = window.innerHeight * 0.6;
-
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const { top } = section.getBoundingClientRect();
-      if (top < this.alturaMedia) {
-        section.classList.add('ativo');
-      } else if (top > this.alturaMedia) {
-        section.classList.remove('ativo');
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.offset > 0) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
+  getDistance() {
+    this.distance = [...this.sections].map((item) => {
+      const offset = item.offsetTop;
+      return {
+        element: item,
+        offset: offset - this.alturaMedia,
+      };
+    });
+  }
+
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    this.getDistance();
+    this.checkDistance();
+    window.addEventListener('scroll', this.checkDistance);
+    return this;
+  }
+
+  stop() {
+    this.sections.forEach((section) => {
+      section.classList.add('ativo');
+    });
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
